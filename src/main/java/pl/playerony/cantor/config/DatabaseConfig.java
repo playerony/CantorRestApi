c4718e0b45500f9a30a7cb020008d5796e2bd084
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-03 10:39:58 +0100

    Create default application config class

---
+++ src/main/java/pl/playerony/cantor/config/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package pl.playerony.cantor.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+	@Autowired
+	private Environment enviroment;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(enviroment.getProperty("db.driver"));
+		dataSource.setUrl(enviroment.getProperty("db.url"));
+		dataSource.setUsername(enviroment.getProperty("db.username"));
+		dataSource.setPassword(enviroment.getProperty("db.password"));
+
+		return dataSource;
+	}
+
+}
