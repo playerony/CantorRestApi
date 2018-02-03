e454c7bc77f6546a401575aedb345fbb0286c7ed
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-03 13:41:23 +0100

    Create UserRepository interface and implementation

---
+++ src/main/java/pl/playerony/cantor/database/repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package pl.playerony.cantor.database.repositories;
+
+import java.util.List;
+
+import pl.playerony.cantor.database.models.User;
+import pl.playerony.cantor.exceptions.CantorRestApiException;
+
+public interface UserRepository {
+	void insertUser(User newUser) throws CantorRestApiException;
+
+    void updateUser(Long userId, User user) throws CantorRestApiException;
+
+    User fetchUserByUserId(Long userId) throws CantorRestApiException;
+
+    User fetchUserByUsername(String username) throws CantorRestApiException;
+
+    User fetchUserByEmail(String email) throws CantorRestApiException;
+
+    List<User> fetchAll() throws CantorRestApiException;
+
+    void removeUser(Long userId) throws CantorRestApiException;
+}
---
+++ src/main/java/pl/playerony/cantor/database/repositories/implementations/UserRepositoryImpl.java
@@ -0,0 +1,120 @@
+package pl.playerony.cantor.database.repositories.implementations;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import pl.playerony.cantor.database.models.User;
+import pl.playerony.cantor.database.repositories.UserRepository;
+import pl.playerony.cantor.exceptions.CantorRestApiException;
+
+@Repository
+@Transactional
+public class UserRepositoryImpl implements UserRepository {
+	@PersistenceContext	
+	private EntityManager entityManager;
+	
+	@Override
+	public void insertUser(User newUser) throws CantorRestApiException {
+		try {
+			entityManager.persist(newUser);
+			entityManager.flush();
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by insert user", e);
+		}
+	}
+
+	@Override
+	public void updateUser(Long userId, User user) throws CantorRestApiException {
+		try {
+			User foundUser = fetchUserByUserId(userId);
+			
+			foundUser.setFirstName(user.getFirstName());
+			foundUser.setLastName(user.getLastName());
+			foundUser.setUsername(user.getUsername());
+			foundUser.setPassword(user.getPassword());
+			foundUser.setEmail(user.getEmail());
+			foundUser.setRoleId(user.getRoleId());
+			foundUser.setBalance(user.getBalance());
+			entityManager.flush();
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by update user", e);
+		}
+	}
+
+	@Override
+	public User fetchUserByUserId(Long userId) throws CantorRestApiException {
+		try {
+			User user = entityManager.find(User.class, userId);
+			entityManager.flush();
+			
+			return user;
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by fetching user", e);
+		}
+	}
+
+	@Override
+	public User fetchUserByUsername(String username) throws CantorRestApiException {
+		try {
+			String sql = "FROM User as user WHERE user.username LIKE ?";
+			User user = (User) entityManager.createQuery(sql)
+									 .setParameter(1, username)
+									 .getSingleResult();
+			
+			entityManager.flush();
+			
+			return user;
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by fetching user by username", e);
+		}
+	}
+
+	@Override
+	public User fetchUserByEmail(String email) throws CantorRestApiException {
+		try {
+			String sql = "FROM User as user WHERE user.email LIKE ?";
+			User user = (User) entityManager.createQuery(sql)
+									 .setParameter(1, email)
+									 .getSingleResult();
+			
+			entityManager.flush();
+			
+			return user;
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by fetching user by email", e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> fetchAll() throws CantorRestApiException {
+		try {
+			String sql = "FROM User as user ORDER BY user.userId";
+			List<User> users = (List<User>) entityManager.createQuery(sql)
+									 			   		 .getResultList();
+			
+			entityManager.flush();
+			
+			return users;
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by fetching users", e);
+		}
+	}
+
+	@Override
+	public void removeUser(Long userId) throws CantorRestApiException {
+		try {
+			User user = fetchUserByUserId(userId);
+			entityManager.remove(user);
+			entityManager.flush();
+		} catch(Exception e) {
+			throw new CantorRestApiException("There's a problem by delete user", e);
+		}
+	}
+
+}
